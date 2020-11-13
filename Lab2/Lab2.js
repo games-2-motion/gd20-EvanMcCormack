6d63c5f6bd1134108fb2c0833526b481a0247d7f
Evan McCormack <34337837+EvanMcCormack@users.noreply.github.com>
2020-11-13 11:38:05 +0000

    Lab 2 part 1 Completed

--- Lab2/Lab2.js
+++ Lab2/Lab2.js
@@ -13,4 +13,26 @@ function main()
 	{
 		console.log("square FAIL");
 	}
+
+	if(howManyLightsabersDoYouOwn("Zach")==18)
+	{
+		console.log("Part 1 OK");
+	}
+	else
+	{
+		console.log("Part 1 FAIL");
+	}
+}
+
+//Part 1
+function howManyLightsabersDoYouOwn(name)
+{
+	if(name === "Zach")
+	{
+		return 18;
+	}
+	else
+	{
+		return 0;
+	}
 }
